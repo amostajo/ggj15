5f48f6a0b0797f9e6c8772d8567cea460bbb5d04
Tolkerism <danielvuurman@hotmail.com>
2015-01-24 07:14:26 -0600

    Scripts For Repetitive Mini Games
    PushingFinger is the object script
    Game manager repetitive is the GM for this PushingFinger game

---
+++ Unity3D/Assets/Scripts/Managers/GameManagerRepetitive.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//@author Daniel Vuurman <danielvuurman@hotmail.com>
+
+public class GameManagerRepetitive : GameManager {
+
+    //Call PushingFinger Script, set pusher
+    private PushingFinger pusher;
+
+    //Direction Vector to return Finger
+    public static Vector3 direction = new Vector3(-1f, 0f, 0f);
+
+    public float force;
+
+    public float forceFactor;
+
+    public override void Awake()
+    {
+        base.Awake();
+        pusher = FindObjectOfType<PushingFinger>();
+    }
+
+    public override void Start()
+    {
+        inputs.RequestKey();
+    }
+
+    //Update checks for Key sent and Key Pressed
+    //Ends game on mistake
+    public void Update()
+    {
+        if (!GameManager.paused && this.inputs.keyCheck) {
+            if (this.inputs.keySuccess)
+            {
+                AddScore(1);
+                if (pusher && pusher.rigidbody)
+                {
+                    pusher.rigidbody.AddForce(direction*force);
+                    force += force*forceFactor;
+                }
+            }
+            else
+            {
+                Finish();
+            }
+        }
+      
+    }
+
+
+}
