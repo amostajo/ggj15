718b1365ef08726dfeecf34b460cc33a7fb23119
Alejandro Mostajo <amostajo@amsgames.com>
2015-01-24 00:35:13 -0600

    Update to game manager

---
+++ Unity3D/Assets/Scripts/Helpers/Timer.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * GameObject script. Script to be attached to a hidden gameobject in the game scene. 
+ * Controls and hangles game time.
+ *
+ * @author Alejandro Mostajo <amostajo@amsgames.com>
+ * @copyright 2014 Amsgames, LLC
+ */
+
+public class Timer : MonoBehaviour {
+  /**
+   * Current game time. (Paused time is subtracted)
+   */
+  public static float time;
+  /**
+   * Current game time without pause subtraction.
+   */
+  public static float unpausedTime;
+  /**
+   * Time loaded from save file. Helps adjust timer time related variables in game objects.
+   */
+  public static float loadedTime = 0f;
+  /**
+   * Flag that indicates if the timer is paused.
+   */
+  [HideInInspector]
+  public bool paused;
+  /**
+   * Amount of paused time.
+   */
+  private float pausedTime;
+  /**
+   * Time when the timer was paused.
+   */
+  private float timeWhenPaused;
+  /**
+   * Creation time.
+   */
+  [HideInInspector]
+  public float creationTime;
+
+  /**
+	 * Unity start
+	 */
+  public void Start () {
+    paused = false;
+    creationTime = pausedTime = 0.0f;
+  }
+
+  /**
+	 * Unity update
+	 */
+  private void Update () {
+    unpausedTime = Time.time - creationTime;
+    if (paused) {
+      pausedTime += Time.time - timeWhenPaused;
+      timeWhenPaused = Time.time;
+    } else {
+      time = Time.time - creationTime - pausedTime;
+    }
+  }
+
+  /**
+   * Pauses the timer.
+   */
+  public void Pause () {
+    paused = true;
+    timeWhenPaused = Time.time;
+  }
+
+  /**
+   * Unpauses the timer.
+   * @function
+   */
+  public void Resume () {
+    paused = false;
+  }
+
+  /**
+   * Gets the game time taking in consideration the paused time.
+   * @function
+   *
+   * @return float Game time
+   */
+  public float GetGameTime () {
+      return time;
+  }
+
+}
--- Unity3D/Assets/Scripts/Managers/GameManager.cs
+++ Unity3D/Assets/Scripts/Managers/GameManager.cs
@@ -14,16 +14,60 @@ public class GameManager : MonoBehaviour {
 	public static bool paused = false;
 
 	/**
+	 * Flag that indicates if game should count game timer.
+	 */
+	public bool hasGameTimer;
+
+	/**
+	 * Global time of the game. Not to use in mini games.
+	 */
+	public float globalTime;
+
+	/**
 	 * Inputs
 	 */
 	[HideInInspector]
 	public InputManager inputs;
 
 	/**
+	 * Score.
+	 */
+	[HideInInspector]
+	public int score;
+
+	/**
+	 * Game time.
+	 */
+	[HideInInspector]
+	public float timer;
+
+	/**
 	 * Unity Awake
 	 */
 	public virtual void Awake () {
-		inputs = FindObjectOfType<InputManager>();
+		this.inputs = FindObjectOfType<InputManager>();
+		this.score = PlayerPrefs.GetInt("score");
+	}
+
+	/**
+	 * Unity start.
+	 */
+	public virtual void Start () {
+		if (hasGameTimer) {
+			this.timer = PlayerPrefs.GetFloat("gameTime");
+		}
+	}
+
+	/**
+	 * Fixed update.
+	 */
+	public virtual void FixedUpdate () {
+		if (hasGameTimer && !GameManager.paused) {
+			this.timer += Time.deltaTime;
+			if (this.timer >= globalTime) {
+				End();
+			}
+		}
 	}
 
 	/**
@@ -37,6 +81,32 @@ public class GameManager : MonoBehaviour {
 	 * Called when game is un paused.
 	 */
 	public virtual void OnUnPause () {
+		// TODO
+	}
+
+	/**
+	 * Adds score points.
+	 *
+	 * @param int points.
+	 */
+	public void AddScore (int points) {
+		this.score += points;
+	}
+
+	/**
+	 * Called when game has finished.
+	 */
+	public virtual void Finish () {
+		PlayerPrefs.SetInt("score", score);
+		if (hasGameTimer) {
+			PlayerPrefs.SetFloat("gameTime", timer);
+		}
+	}
+
+	/**
+	 * To be called when game is over..
+	 */
+	public virtual void End () {
 		// TODO
 	}
 }
